57e875dbc2805e29248226f2efc565b2701b15a7
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2025-02-24 12:30:22 -0300

    feat: ajusta admin, formulários e views

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,8 +4,16 @@ from django.contrib.auth.models import User
 
 class RegistrarForm(UserCreationForm):
     email = forms.EmailField()
-    nome = forms.CharField(max_lenght=100)
+    nome = forms.CharField(max_length=100)
 
-    clas Meta:
-    model = User
-    fields = ['username', 'nome', 'email', 'password1', 'password2']
+    class Meta:
+        model = User
+        fields = ['username', 'nome', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['nome'] 
+        if commit:
+            user.save()
+        return user
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from .forms import RegistrarForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
 def registrar(request):
+    if request.user.is_authenticated:
+        return redirect('painel') 
+
     if request.method == 'POST':
         form = RegistrarForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, 'Cadastro realizado com sucesso!')
             return redirect('painel')
+        else:
+            messages.error(request, 'Erro ao cadastrar, verifique os dados.')
     else:
         form = RegistrarForm()
-    return render(request, 'registrar.html', {'form: form'})
+    
+    return render(request, 'registrar.html', {'form': form})
 
 def fazer_login(request):
-    pass 
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('painel')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
 def fazer_logout(request):
     logout(request)
